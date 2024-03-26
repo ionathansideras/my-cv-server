be657b98410e09e23aad32d4a95df677e7b3351b
Ionathan Sideras <iona8ansideras@gmail.com>
2024-03-27 00:24:59 +0200

    Add User and Project types to src/types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,17 @@
+// Define a type for User
 type User = {
     name: string;
     email: string;
     password: string;
-    projects: Project[];
+    projects: Project[]; // Array of projects associated with the user
 };
 
+// Define a type for Project
 type Project = {
     id: string;
     name: string;
     description: string;
 };
 
+// Export the User type so it can be used in other parts of the application
 export { User };
